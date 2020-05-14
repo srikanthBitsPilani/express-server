7d7b4939698453c049d34ce492949eaa8d33b693
Sri <srikaanth.00@gmail.com>
2020-05-14 12:36:06 +0530

    express serving static files

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = "3000";
 
 const app = express();
 
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
+
 app.use((req, res, next) => {
-  console.log(req.header);
   res.setHeader("Content-Type", "text/html");
   res.statusCode = 200;
   res.end("<html><body><h1>Hello World</h1></body></html>");
